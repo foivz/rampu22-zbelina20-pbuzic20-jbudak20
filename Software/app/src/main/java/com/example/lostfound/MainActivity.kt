6e650f370cd8dd3255069a43c97ef85e6d1cdbbc
pbuzic20 <pbuzic20@foi.hr>
2022-11-14 23:06:17 +0100

    Implementirano povezivanje aktivnosti Klikom na gumb na glavnoj aktivnosti, otvori se aktivnost za kreiranja objava.

--- Software/app/src/main/java/com/example/lostfound/MainActivity.kt
+++ Software/app/src/main/java/com/example/lostfound/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.lostfound
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fabCreate = findViewById<FloatingActionButton>(R.id.fabCreate)
+        fabCreate.setOnClickListener{
+            val intent = Intent(this, CreateActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
